a4812da9a8b9480b406609b35774706117253d78
Jérôme GIL <linkjerome@gmail.com>
2020-02-16 00:13:30 +0100

    Added LeafletMap

--- src/layouts/Dashboard.js
+++ src/layouts/Dashboard.js
@@ -1,17 +1,33 @@
-import React, {Fragment} from 'react';
-import {Container, Notification} from "react-bulma-components";
-import {withTranslation} from "react-i18next";
+import React from 'react';
+import {Hero} from "react-bulma-components";
+import 'leaflet/dist/leaflet.css'
+import {Map, TileLayer} from 'react-leaflet';
 
-function Dashboard({t}) {
-    return (
-        <Fragment>
-            <Container breakpoint="fullhd">
-                <Notification>
-                   Welcome, <strong>name</strong>. Are you ready to <code>code</code> ?
-                </Notification>
-            </Container>
-        </Fragment>
-    )
+export default class Dashboard extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            lat: 51.505,
+            lng: -0.09,
+            zoom: 13
+        };
+    }
+
+    componentDidMount() {
+        console.log(this.props, this.state);
+    }
+
+    render() {
+        const position = [this.state.lat, this.state.lng];
+
+        return (
+            <Hero size="fullheight">
+                <Map center={position} zoom={this.state.zoom}>
+                    <TileLayer
+                        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                        url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+                    />
+                </Map>
+            </Hero>);
+    }
 }
-
-export default withTranslation()(Dashboard)
