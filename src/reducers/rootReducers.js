71ad546ed0d1362a36fa81bdee41c88a78136b4f
Jérôme GIL <linkjerome@gmail.com>
2020-07-05 22:58:47 +0200

    Ma(p)ssive Improvement

--- src/reducers/rootReducers.js
+++ src/reducers/rootReducers.js
@@ -1,22 +1,6 @@
-import * as MAPMODES from '../assets/constants/mapModes';
-
 // Lets assume it's the global state (you can add field here)
 const initialState = {
     openDrawer: false,
-    mobile: false,
-    map: undefined,
-    paths: [],
-    layersControl: undefined,
-    savedState: {
-        paths: [],
-        undo: false,
-        upload: false
-    },
-    layers: [],
-    markersColor: [],
-    tempMarkers: [],
-    focus: undefined,
-    mode: MAPMODES.MOVEMAP
 };
 
 // Add your action type here + describe what it does functionally (don't forget to add '...state' to get other fields)
@@ -27,11 +11,6 @@ export default function rootReducers(state = initialState, action) {
             return {
                 ...state,
                 openDrawer: !state.openDrawer
-            };
-        case "CHANGEMODE":
-            return {
-                ...state,
-                mode: action.mode === state.mode ? MAPMODES.MOVEMAP : action.mode
             };
         default:
             return state;
