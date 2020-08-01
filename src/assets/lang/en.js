a19688fedc322600e37a772867a06915d9d43030
Jérôme GIL <LinkJerome@gmail.com>
2020-08-01 13:09:20 +0200

    Created loading loading-page

--- src/assets/lang/en.js
+++ src/assets/lang/en.js
@@ -1,19 +1,20 @@
 export const en = {
-    "siteName": "Improve my GPX",
-    "contributors": "Contributors",
-    "goBack": "Go back to the previous page",
-    "goToHome": "Go to the home page",
-    "homeDescription": "Improve my GPX was firstly a website made for a universitary project. It has been rebuilt and rethought to become a webapp.",
-    "homeGoToMap": "Go to the map",
-    "homeSubtitle": "Biking, hiking lovers ...?",
-    "pageNotFoundTitle": "Oops... Page not found",
-    "pageNotFoundContent": "The page you are looking for does not seem to exist.",
-    "welcome": "Welcome",
-    "sidebar": {
-        "menuItems": {
-            "1": "Tutorial",
-            "2": "Need help ?",
-            "3": "About",
+    'siteName': 'Improve my GPX',
+    'contributors': 'Contributors',
+    'goBack': 'Go back to the previous page',
+    'goToHome': 'Go to the home page',
+    'homeDescription': 'Improve my GPX was firstly a website made for a universitary project. It has been rebuilt and rethought to become a webapp.',
+    'homeGoToMap': 'Go to the map',
+    'homeSubtitle': 'Biking, hiking lovers ...?',
+    'loading': 'Loading...',
+    'pageNotFoundTitle': 'Oops... Page not found',
+    'pageNotFoundContent': 'The page you are looking for does not seem to exist.',
+    'welcome': 'Welcome',
+    'sidebar': {
+        'menuItems': {
+            '1': 'Tutorial',
+            '2': 'Need help ?',
+            '3': 'About',
         },
     },
 };
